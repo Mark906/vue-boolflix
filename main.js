d7ba597462a9df538b5f97eb0e54ab030a3115e1
Marco <markoka1@outlook.it>
2020-11-25 21:27:29 +0100

    piccoli fix

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const api_url_base = 'https://api.themoviedb.org/3/';
 const api_key = 'bd2a9dc4670609b1dcc9d5fd6281d9bc';
-const casualsearch = Math.floor(Math.random() * 5) + 1
+const casualsearch = Math.floor(Math.random() * 4)
 const film = ["pippo", "natale", "estate", "inverno", "horror"];
-
 var app = new Vue({
     el: '#root',
     data: {
         movie: []
+        //length: 0
     },
 
     mounted() {
@@ -20,6 +20,8 @@ var app = new Vue({
                 console.log(risposta);
                 this.movie = risposta.data.results;
                 console.log(this.movie)
+                //this.length = this.movie.length;
+                //console.log(this.length);
             });
     },
     methods: {
@@ -27,5 +29,13 @@ var app = new Vue({
             return Math.round((this.movie[index].vote_average) / 2);
 
         }
+
+        //movielength(){
+            //let bool = true;
+            //setTimeout(function(){
+                //if(this.length == 0){bool = false; }
+            //}, 1000);
+            //return bool;
+        //}
     }
 });
