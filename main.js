e37c6e0c2d98b46043507b9e2840a2b240228f57
Marco <markoka1@outlook.it>
2020-11-26 16:01:53 +0100

    gestito caso in cui non vi sono risultati

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
 const api_url_base = 'https://api.themoviedb.org/3/';
 const api_key = 'bd2a9dc4670609b1dcc9d5fd6281d9bc';
-const casualsearch = Math.floor(Math.random() * 4)
-const film = ["pippo", "natale", "estate", "inverno", "horror"];
 var app = new Vue({
     el: '#root',
     data: {
         movie: [],
         //length: 0
-        languages: ["de", "en", "fr", "it", "pt"]
-    },
-
-    mounted() {
-
-        axios.get(api_url_base + 'search/movie', {
-            params: {
-                api_key: api_key,
-                query: film[casualsearch]
-            }
-        }).then((risposta) => {
-                console.log(risposta);
-                this.movie = risposta.data.results;
-                console.log(this.movie)
-                //this.length = this.movie.length;
-                //console.log(this.length);
-            });
+        languages: ["de", "en", "fr", "it", "pt"],
+        isRicercaFatta : false,
+        searchInput : '  '
     },
     methods: {
+        ricerca() {
+
+            var ricerca = this.searchInput;
+
+            axios.get(api_url_base + 'search/movie', {
+                params: {
+                    api_key: api_key,
+                    query: ricerca
+                }
+            }).then((risposta) => {
+
+                    this.isRicercaFatta = true;
+
+                    console.log(risposta);
+                    this.movie = risposta.data.results;
+                    console.log(this.movie)
+                    //this.length = this.movie.length;
+                    //console.log(this.length);
+                });
+        },
         calcolaStars(index){
-            return Math.round((this.movie[index].vote_average) / 2);
+            return Math.ceil((this.movie[index].vote_average) / 2);
 
         },
 
@@ -40,12 +44,5 @@ var app = new Vue({
             }
             return bool = false;
         }
-        //movielength(){
-            //let bool = true;
-            //setTimeout(function(){
-                //if(this.length == 0){bool = false; }
-            //}, 1000);
-            //return bool;
-        //}
     }
 });
