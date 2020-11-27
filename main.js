af08e3c5523e264dcf70b712f868b4a40958156e
Marco <markoka1@outlook.it>
2020-11-27 16:30:50 +0100

    Aggiunta di tutti i poster + caso in cui il poster non c'è

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const api_url_base = 'https://api.themoviedb.org/3/';
 const api_key = 'bd2a9dc4670609b1dcc9d5fd6281d9bc';
-const url_base_locandina = 'https://image.tmdb.org/t/p/';
-const dimensione_locandina = 'w185';
+const url_base_poster = 'https://image.tmdb.org/t/p/';
+const dimensione_poster = 'w185';
+const poster_not_avaiable = 'img/poster_not_avaiable.jpg'
 var app = new Vue({
     el: '#root',
     data: {
         movie: [],
-        //length: 0
         languages: ["de", "en", "fr", "it", "pt"],
         isRicercaFatta : false,
         searchInput : '  '
@@ -28,8 +28,7 @@ var app = new Vue({
                     console.log(risposta);
                     this.movie = risposta.data.results;
                     console.log(this.movie)
-                    //this.length = this.movie.length;
-                    //console.log(this.length);
+
                 });
 
             axios.get(api_url_base + 'search/tv', {
@@ -44,8 +43,7 @@ var app = new Vue({
                     console.log(risposta);
                     this.movie = risposta.data.results;
                     console.log(this.movie)
-                        //this.length = this.movie.length;
-                        //console.log(this.length);
+
                 });
         },
         calcolaStars(index){
@@ -61,6 +59,15 @@ var app = new Vue({
                 }
             }
             return bool = false;
+        },
+
+        get_url_poster(path) {
+            if(path) { //se il valore esiste
+                //ritorna poster
+                return url_base_poster + dimensione_poster + path;
+            }
+            //altrimenti ritorna poster non disponibile
+            return poster_not_avaiable;
         }
     }
 });
