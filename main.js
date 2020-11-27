faf3d21c8c6694428f1640e60b44bb74c4823a21
Marco <markoka1@outlook.it>
2020-11-27 16:03:52 +0100

    aggiunte serie tv + inizializzazione milestone 3

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const api_url_base = 'https://api.themoviedb.org/3/';
 const api_key = 'bd2a9dc4670609b1dcc9d5fd6281d9bc';
+const url_base_locandina = 'https://image.tmdb.org/t/p/';
+const dimensione_locandina = 'w185';
 var app = new Vue({
     el: '#root',
     data: {
@@ -28,6 +30,22 @@ var app = new Vue({
                     console.log(this.movie)
                     //this.length = this.movie.length;
                     //console.log(this.length);
+                });
+
+            axios.get(api_url_base + 'search/tv', {
+                params: {
+                    api_key: api_key,
+                    query: ricerca
+                }
+            }).then((risposta) => {
+
+                    this.isRicercaFatta = true;
+
+                    console.log(risposta);
+                    this.movie = risposta.data.results;
+                    console.log(this.movie)
+                        //this.length = this.movie.length;
+                        //console.log(this.length);
                 });
         },
         calcolaStars(index){
